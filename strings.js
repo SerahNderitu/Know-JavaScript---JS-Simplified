8f2dff638bb3f2b05caa5db90664eba61bfe1d49
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-25 10:24:17 +0300

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+<!-- count all characters and return a value -->
+  "superhuman".length // 10
+
+<!-- count 10 letters plus the space, making the length to be 11-->
+"some string".length // 11
+
+<!-- a space is regarded as a character, hence length 1-->
+" ".length // 1
+  
+<!-- no space -->
+  "".length // 0
+
+<!-- checking whether a string include a value-->
+
+  "Birthday".includes("day") // true
+
+  "JavaScript".includes("doc") // false
