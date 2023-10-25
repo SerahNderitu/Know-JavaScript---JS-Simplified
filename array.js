9a78a24da7c5645d2d3dc2ee29bce94683da9904
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-25 14:07:09 +0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,33 @@
+<!--empty array-->
+
+  let emptyArray = [ ];
+
+<!-- one item array -->
+
+  let oneItemArray = ["jQuery"];
+
+<!--mixed items array-->
+
+  let mixedItems = ["code", 2' "you", false, document];
+
+
+<!--array sort method-->
+
+  ("c", "a", "b").sort()
+        // "a", "b", "c"
+
+<!-- array join method-->
+
+  ("JavaScript", "Python", "Django").join(" & ");
+          // "JavaScript & Python & Django"
+
+<!-- array concate method-->
+
+  [1,2,3].concat([4,5,6])
+      // [1,2,3,4,5,6]
+
+<!-- array push method -->
+
+  [1,3,3].push(4)
+      // 1,2,3,4
+  
