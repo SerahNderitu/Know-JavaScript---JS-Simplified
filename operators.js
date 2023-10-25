0893fa22a66326641740fd797640ff4e66520ca4
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-25 11:26:45 +0300

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,16 @@
+<!-- + concate/combine/merge strings-->
+  "1" + "1" = "11"
+
+<!-- used in numbers for addition-->
+   5 + 4; // 9
+
+<!-- strict operator-->
+  1 === 1 // true
+<!-- but -->
+  1 === "1"  // false
+
+<!-- with loosey-goosey operator-->
+  1 == 1  // true
+
+  1 == "1"  // true which is wrong
+
