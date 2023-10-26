6b3a1fe6ef4d407aabdd2da81f77948af206845d
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 08:31:25 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,17 @@
+// function example1
+
+const greeting = function(saying) {
+  return saying.toUpperCase();
+};
+console.log(greeting("Hello, friend"));
+
+
+// function example2
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Calling the function
+greet("Ryan"); // Hello, Ryan!
+greet("Sarah"); // Hello, Sarah!
