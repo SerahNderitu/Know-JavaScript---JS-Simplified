823a9fd0f10b44beb7f061fcd9ec7abffe5d9025
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:43:33 +0300

    Update expression.js

--- expression.js
+++ expression.js
@@ -1,12 +1,12 @@
-<!-- an expression that evaluates to a boolean-->
+// an expression that evaluates to a boolean
    
    "codemaster".includes("code")  // true
 
 
-<!-- an expression that resolves to a value -->
+// an expression that resolves to a value 
   
    5 * 10   // 50
 
-<!-- string expression -->
+// string expression 
    
    "Birthday" + " " + "party"  // Birthday party
