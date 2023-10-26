d40bfbef3414d29c6dc402a11f663035ff4b31c8
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:45:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-<!-- find index of m-->
+// find index of m
 "document".indexOf("m") // 4
 
-<!-- a word with the same characters, indexOf returns the first letter-->
+// a word with the same characters, indexOf returns the first letter
 "process".indexOf("s") // 5
 
-<!-- returns -1 for the character that doesn't exist-->
+// returns -1 for the character that doesn't exist
 "code".indexOf("m") // -1
