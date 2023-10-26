3b24cce75fd786875e07abad625e55c1049930f8
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:46:01 +0300

    Update modifywebpage.js

--- modifywebpage.js
+++ modifywebpage.js
@@ -1,5 +1,5 @@
-<!-- changes the title on a web page to 'my favorite page' -->
+// changes the title on a web page to 'my favorite page'
   document.title = "my favorite page"
 
-<!-- adds **&friends** to the text element with a class = "name"  -->
+// adds **&friends** to the text element with a class = "name" 
   document. getElementByClass("name").append("& friends")
