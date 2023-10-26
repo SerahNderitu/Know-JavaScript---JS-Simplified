a9dcb54041126534fc1853a8741fa38c3fe9adb3
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:44:40 +0300

    Update findelements.js

--- findelements.js
+++ findelements.js
@@ -1,11 +1,11 @@
-<!-- pick the elements within the id labeled as mark -->
+// Pick the elements within the id labeled as mark 
 document.getElementById("mark")
 
-<! picks all the h1 elements -->
+// picks all the h1 elements 
   document.getElementByTagName("h1")
 
-<!-- picks all elements within class student -->
+// picks all elements within the class student 
   document.getElementByClassName("student")
 
-<!-- returns a collection of elements that match a CSS selector with the name school -->
+// returns a collection of elements that match a CSS selector with the name school 
   document.querySelectorAll(".school")
