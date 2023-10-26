965d71a31460435201a97ba42c3a055b6b3caab8
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:41:05 +0300

    Update operators.js

--- operators.js
+++ operators.js
@@ -1,30 +1,31 @@
-<!-- + concate/combine/merge strings-->
+// + concate/combine/merge strings
   "1" + "1" = "11"
 
-<!-- used in numbers for addition-->
+// used in numbers for addition-->
    5 + 4; // 9
 
-<!-- strict operator-->
+// strict operator
   1 === 1 // true
-<!-- but -->
+
+// but
   1 === "1"  // false
 
-<!-- with loosey-goosey operator-->
+// with loosey-goosey operator
   1 == 1  // true
 
   1 == "1"  // true which is wrong. JS converts them to the same type, to look the same.
 
-<!-- examples unary operator. They have only one operand -->
+// examples unary operator. They have only one operand 
     -1, +1, !5
 
-<!--binary operators -->
+// binary operators 
     5 + 4  // 9
 
-<!--ternary operators-->
+// ternary operators
 
     let feeling = taste === "yummy" ? "happy": "sad";
 
-<!-- Is a shorthand way to write this code -->
+// Is a shorthand way to write this code 
   if(taste === "yummy") {
       mood = "happy";
   }else{
