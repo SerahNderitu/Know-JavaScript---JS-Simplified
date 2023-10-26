bf91e0a4c002d079b009d9f001e23d11f9d2fced
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 12:34:56 +0300

    Create map.js

---
+++ map.js
@@ -0,0 +1,9 @@
+
+const music = [
+  { song: "Baby I", artist:" Ariana"},
+  { song: "Gambler", artist:"Kenny"},
+  {song: "Shower", artist: "Becky"}
+  ];
+  const artists = music.map(s => s.artist + " Music ");  // ["Ariana Music", "Kenny Music", "Becky Music"]
+
+// the new array will have artist with music at the end
