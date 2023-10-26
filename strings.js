ad56b861c1e7ad728626fb38160bb82834be7a91
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 06:48:29 +0300

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,16 +1,16 @@
-<!-- count all characters and return a value -->
+// Count all characters and return a value
   "superhuman".length // 10
 
-<!-- count 10 letters plus the space, making the length to be 11-->
+// Count 10 letters plus the space, making the length to be 11
 "some string".length // 11
 
-<!-- a space is regarded as a character, hence length 1-->
+// A space is regarded as a character, hence length of 1
 " ".length // 1
   
-<!-- no space -->
+// no space 
   "".length // 0
 
-<!-- checking whether a string include a value-->
+// checking whether a string includes a value
 
   "Birthday".includes("day") // true
 
