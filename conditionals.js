fa710bb3e61c6a646e7008a28b881fc2506ea6de
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 12:03:59 +0300

    Create conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,31 @@
+// The code below will learn if the condition is true.
+
+if(you.wannaBeMyFriend) {
+  you.gottaLoveJs = true;
+}
+
+
+// We use else to execute the other code if the condition is false
+
+if(you.reallyNag(me) {
+  me.say("You can't be my friend");
+} else {
+  me.say{"Hello bestie");
+        }
+
+if (5 > 10) {
+  alert(" 5 is greater than 10");
+} else {
+  alert(" 5 is always less than 10");
+}
+
+
+// using if, else if, and else in the same block of code
+
+if (5 > 10) {
+  alert(" 5 is greater than 10");
+} else if(5 === 5) {
+  alert("5 is equal to 5");
+} else {
+  alert(" 5 can be 10");
+}
