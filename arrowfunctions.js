b54551b306b7526df6176c863b0aa4448c6a1a26
SerahNderitu <42427921+SerahNderitu@users.noreply.github.com>
2023-10-26 08:48:00 +0300

    Create arrowfunctions.js

---
+++ arrowfunctions.js
@@ -0,0 +1,13 @@
+// arrow function example used to create little functions
+
+(x,y) => x + y;
+
+// arrow functions assigned to a variable
+
+const add = (x,y) => (x + y);
+
+//  before arrow funtion, the above expression would be like
+
+function add(x,y) {
+  return x + y;
+}
